874c5f84c797347991e6a2078374d923ee544cb9
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-14 14:45:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number / 3 && !number / 5
+  if number / 3 && number !/ 5
     "Fizz"
-  elsif number % 5 && !number / 3
+  elsif number % 5 && !(number / 3)
     "Buzz"
   elsif number / 5 && number / 3
     "FizzBuzz"
